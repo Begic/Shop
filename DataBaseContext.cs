93b8aa1a28dc71fb6643555bb4f405af04540639
Süleyman Begic <begic@software.tirol>
2023-11-29 09:14:37 +0100

    add dbset

--- DataBaseContext.cs
+++ DataBaseContext.cs
@@ -8,8 +8,11 @@ namespace Shop
     {
         
         public DbSet<Product> Products { get; set; }
-
-
-
+        public DbSet<PriceListPosition> PriceListPositions { get; set; }
+        public DbSet<PriceList> PriceLists { get; set; }
+        public DbSet<Currency> Currencies{ get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<CustomerState> CustomerStates { get; set; }
+        public DbSet<CustomerType> CustomerTypes { get; set; }
     }
 }
